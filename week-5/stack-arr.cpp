fe29edff338c5e8d93b63e20e405fbb9bb88545a
Hammad Majid <hammadmajid@proton.me>
2025-03-13 12:52:44 +0500

    stack array

---
+++ week-5/stack-arr.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#define MAX 5
+
+using namespace std;
+
+int stack[MAX], top;
+
+void initStack() { top = -1; }
+
+bool isEmpty() { return top == -1; };
+
+int isFull() { return top == MAX; }
+
+void push(int data) {
+  if (isFull()) {
+    cout << "Stack overflow" << endl;
+  } else {
+    top += 1;
+    stack[top] = data;
+  }
+}
+
+void pop() {
+  if (isEmpty()) {
+    cout << "Stack underfow" << endl;
+  } else {
+    stack[top] = 0;
+    top -= 1;;
+  }
+}
+
+void display() {
+  for (int i = 0; i <= top; i++) {
+    cout << stack[i] << endl;
+  }
+}
+
+int main() {
+  initStack();
+
+  push(1);
+  push(2);
+  pop();
+  push(3);
+
+  display();
+}
+

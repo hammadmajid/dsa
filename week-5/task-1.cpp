8bf1faeef6383231ae2d4d91d6a112d197835dbd
Hammad Majid <hammadmajid@proton.me>
2025-03-13 13:27:14 +0500

    add task 1

---
+++ week-5/task-1.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#define MAX 5
+
+using namespace std;
+
+char stack[MAX];
+int top;
+
+void initStack() { top = -1; }
+
+bool isEmpty() { return top == -1; };
+
+bool isFull() { return top == MAX - 1; }
+
+void push(char data) {
+  if (isFull()) {
+    cout << "Stack overflow" << endl;
+  } else {
+    top += 1;
+    stack[top] = data;
+  }
+}
+
+void pop() {
+  if (isEmpty()) {
+    cout << "Stack underfow" << endl;
+  } else {
+    stack[top] = ' ';
+    top -= 1;
+  }
+}
+
+void display() {
+  for (int i = 0; i <= top; i++) {
+    cout << stack[i] << endl;
+  }
+}
+
+int main() {
+  initStack();
+
+  push('a');
+  push('b');
+  pop();
+  push('c');
+
+  display();
+}

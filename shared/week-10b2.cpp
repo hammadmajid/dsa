2f45cce853e91bca6cefeec82a7953b46d7867c0
Hammad Majid <hammadmajid@proton.me>
2025-04-23 10:46:56 +0500

    add week 10 shared code

---
+++ shared/week-10b2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+struct Node {
+  int data;
+  Node *left;
+  Node *right;
+};
+
+int maxDepth(Node *root) {
+  // Root being null means tree doesn't exist.
+  if (root == NULL)
+    return 0;
+
+  // Get the depth of the left and right subtree
+  // using recursion.
+  int leftDepth = maxDepth(root->left);
+  int rightDepth = maxDepth(root->right);
+
+  // Choose the larger one and add the root to it.
+  if (leftDepth > rightDepth)
+    return leftDepth + 1;
+  else
+    return rightDepth + 1;
+}
+
+int main() {}
